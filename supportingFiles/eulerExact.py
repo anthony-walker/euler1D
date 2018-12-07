Working on merging contents of sodshock into eulerExact that way one file will control both solutions and avoid redundancy sodshock still needs fixed and numerical solution needs fixed also read me needs updated

supportingFiles/eulerExact.py
@@ -2,6 +2,7 @@
 import numpy as np
 import datetime as dt
 import scipy.optimize
+
 #Allocation of global variables
 gamma = 0
 alpha = 0
@@ -13,10 +14,17 @@ rbc = 0
 t = 0
 numPts = 0
 epsilon = 0
-
-def eulerExact(fileName,fileStoreInfo,time = 0.2,lBC = (1.0,1.0,0,2.5),rBC = (0.1,0.125,0,0.25),g = 1.4,npts = 1000):
-    """Use this method to determine the analytic solution of sod shock problem."""
+def solveAnalytical(fileName,fileStoreInfo,time = 0.2,lBC = (1.0,1.0,0,2.5),rBC = (0.1,0.125,0,0.25),g = 1.4,npts = 1000,sol = True):
+    """Use this method to solve the exact euler equations or the analytical sod shock solution."""
+    #Assigns global variables for solving
     gVI(g,rBC,lBC,time,npts)
+    if sol:
+        eulerExact(fileName,fileStoreInfo)
+    else:
+        sodShock(fileName,fileStoreInfo)
+
+def eulerExact(fileName,fileStoreInfo):
+    """Use this method to determine the analytic solution of sod shock problem."""
     #Determining states
     statesList = states2and3()
     rho = statesList[0]
@@ -122,6 +130,107 @@ def RankineHugoniot(P):
     temp = (1-temp)**beta
     # return P
     return temp*lbc[0]-P
+
+def sodShock(fileName,fileStoreInfo):
+    """Use this method to determine the analytic solution of sod shock problem."""
+    #Important coeffcients
+    Gamma = (gamma-1)/(gamma+1)
+    #Determining states
+    statesList = states(Gamma,lbc,rbc)
+    rho = statesList[0]
+    u = statesList[1]
+    p = statesList[2]
+
+    #Speed of sound for state 3
+    c3 = cL-(gamma-1)/2*u[2]
+
+    #Location
+    x1 = 0
+    x2 = x1-cL*t
+    x3 = x1 - (u[2]-c3)*t
+    x4 = x1 + u[2]*t
+    x5 = x1 + u[3]*t
+    x = [x1,x2,x3,x4,x5]
+
+    #Getting Data
+    xI = np.linspace(-0.5,0.5,npts)
+    pD = np.zeros(npts)
+    rhoD = np.zeros(npts)
+    uD = np.zeros(npts)
+    combList = list()
+    for i in range(npts):
+        if(xI[i]<x1):
+            rhoD[i] = rho[0]
+            pD[i] = p[0]
+            uD[i] = u[0]
+        elif(xI[i] < x2 and xI[i] > x1):
+            rhoD[i] = rho[1]
+            pD[i] = p[1]
+            uD[i] = u[1]
+        elif(xI[i] < x3 and xI[i] > x2):
+            rhoD[i] = rho[2]
+            pD[i] = p[2]
+            uD[i] = u[2]
+        elif(xI[i] < x4 and xI[i] > x3):
+            rhoD[i] = rho[3]
+            pD[i] = p[3]
+            uD[i] = u[3]
+        elif(xI[i] > x4):
+            rhoD[i] = rho[4]
+            pD[i] = p[4]
+            uD[i] = u[4]
+        eTemp = eqnState(pD[i],rhoD[i],uD[i],gamma)
+        combList.append([pD[i],rhoD[i],uD[i],eTemp])
+    analytSolFile(combList,fileName,fileStoreInfo)
+
+def states(Gamma):
+    "Use this method to solve for the states"
+    # Left side conditions
+    pL = lbc[0]
+    rhoL = lbc[1]
+    uL = lbc[2]
+    # Right side conditions
+    pR = rbc[0]
+    rhoR = rbc[1]
+    uR = rbc[2]
+    #iteratively solving
+    tempList = pState3(Gamma)
+    #Pressure
+    p2 = pL*(1-(gamma-1)/2*tempList[1]/cL)**(2*gamma/(gamma-1))
+    p3 = tempList[0]
+    p4 = p3
+
+    #Velocity
+    u2 = tempList[1]
+    u4 = tempList[2]
+    u3 = uR+(p3-pR)/np.sqrt(rhoR/2*((gamma+1)*p3+(gamma-1)*pR))
+
+    #Density
+    rho2 = rhoL*(1-(gamma-1)/2*u2/cL)**(2/(gamma-1))
+    rho3 = rhoL*(p3/pL)**(1/gamma)
+    rho4 = rhoR*(p4+Gamma*pR)/(pR+p4*Gamma)
+
+    rho = [rhoL,rho2,rho3,rho4,rhoR]
+    u = [uL,u2,u3,u4,uR]
+    p = [pL,p2,p3,p4,pR]
+    return [rho, u, p]
+
+def pState3(Gamma):
+    """Use this method to iterate for pressure at state 3."""
+    rP = [rBC[0],lBC[0]]
+    bool =  True
+    while(bool): #Loop to solve pressure iteratively
+        pGuess = (rP[0]+rP[1])/2
+        u4 = (pGuess-rbc[0])*np.sqrt((1-Gamma)/(rbc[1]*(pGuess+Gamma*rbc[0])))
+        u2 = ((lbc[0]**beta-pGuess**beta)
+            *np.sqrt((1-Gamma**2)*lbc[0]/(Gamma**2*lbc[1])))
+        if(abs(u2-u4)<1e-15):
+            bool = False
+        elif(u2-u4 > 0):
+            rP[0] = pGuess
+        elif(u2-u4<0):
+            rP[1] = pGuess
+    return [pGuess,u2,u4]
 
 def gVI(g,rBC,lBC,time,npts):
     """Use this method to initialize global variables."""